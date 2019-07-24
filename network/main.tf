470d274d99aee1dd249b5510df3c4ad41f3c90a3
Ameer Deen <ameer.deen@cba.com.au>
2019-07-24 16:14:21 +1000

    Separating network and server creations

---
+++ network/main.tf
@@ -0,0 +1,44 @@
+provider "azurerm" {}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}"
+  location = "${var.region}"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+}
+
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  address_prefix       = "10.0.2.0/24"
+}
+
+
+resource "azurerm_network_security_group" "main" {
+    name                = "${var.prefix}-rdp-nsg"
+    location            = "${var.region}"
+    resource_group_name = "${azurerm_resource_group.main.name}"
+    
+    security_rule {
+        name                       = "RDP"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "3389"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "Staging"
+    }
+}
